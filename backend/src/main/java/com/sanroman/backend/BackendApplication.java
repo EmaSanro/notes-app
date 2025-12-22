efe5e15124102cbc785df63adf5ca5631b35d0d7
Emanuel San Roman <emanuel.sanroman03@gmail.com>
2025-12-22 01:39:15 -0300

    delete dotenv for read .env files

--- backend/src/main/java/com/sanroman/backend/BackendApplication.java
+++ backend/src/main/java/com/sanroman/backend/BackendApplication.java
@@ -3,19 +3,10 @@ package com.sanroman.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		// Automatically searches for the .env file in the working directory (project root)
-		Dotenv dotenv = Dotenv.load(); 
-
-        // It iterates over the loaded variables and sets them as system properties for Spring Boot to read.
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
